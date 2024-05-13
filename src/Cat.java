bf64402e956567a8702bd06184640f06949201be
eunjaelim <imeunjae361@gmail.com>
2024-05-13 15:37:26 +0900

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,8 @@
+public class Cat implements Animal,Pet {
+    public void cry(){
+        System.out.println("야옹~!");
+    };
+    public void play(){
+        System.out.println("쥐잡기");
+    };
+}
---
+++ src/Dog.java
@@ -0,0 +1,8 @@
+class Dog implements Animal,Pet{
+    public void cry(){
+        System.out.println("멍멍!");
+    }
+    public void play(){
+        System.out.println("원반 던지기");
+    };
+}
